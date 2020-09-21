93a6cdd97578f6984c248ef878990d72e2c18e19
abhishekviu <abhishek.gupta@vuclip.com>
2020-09-21 14:33:38 +0530

    1) Added koin integration in application class

---
+++ app/src/main/java/com/abhishekgupta/trending/TrendingApplication.kt
@@ -0,0 +1,24 @@
+package com.abhishekgupta.trending
+
+import android.app.Application
+import com.abhishekgupta.trending.di.networkModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class TrendingApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger()
+            androidContext(this@TrendingApplication)
+            modules(
+                listOf(
+                    networkModule
+                )
+            )
+        }
+    }
+}
