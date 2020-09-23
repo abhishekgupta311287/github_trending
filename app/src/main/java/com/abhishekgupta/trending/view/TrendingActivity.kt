5ba458e7506397fb4450a1948d2436db1cfb3965
abhishekviu <abhishek.gupta@vuclip.com>
2020-09-23 19:32:01 +0530

    Align action bar title to center

--- app/src/main/java/com/abhishekgupta/trending/view/TrendingActivity.kt
+++ app/src/main/java/com/abhishekgupta/trending/view/TrendingActivity.kt
@@ -1,7 +1,14 @@
 package com.abhishekgupta.trending.view
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Typeface
 import android.os.Bundle
+import android.util.TypedValue
+import android.view.Gravity
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.abhishekgupta.trending.R
 
 class TrendingActivity : AppCompatActivity() {
@@ -9,10 +16,31 @@ class TrendingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.trending_activity)
+        setTitleCentered()
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, TrendingFragment.newInstance())
-                    .commitNow()
+                .replace(R.id.container, TrendingFragment.newInstance())
+                .commitNow()
         }
     }
+
+    private fun setTitleCentered() {
+        supportActionBar!!.setHomeButtonEnabled(true)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        val textView = TextView(this)
+        textView.text = resources.getString(R.string.app_name)
+        textView.setPadding(0, 14, 0, 14)
+        textView.typeface = Typeface.DEFAULT_BOLD
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, resources.getDimension(R.dimen.title_size))
+        textView.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        textView.gravity = Gravity.CENTER
+        textView.setTextColor(ContextCompat.getColor(this, R.color.title_color))
+        supportActionBar!!.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+        supportActionBar!!.customView = textView
+    }
+
 }
